36598b86e2aab4f5a39e0ea65631bb2df3ff9610
Faisal Ashfaq <faisal0ashfaq@gmail.com>
2023-07-01 15:52:49 +0530

    Added print message

--- dashboard.py
+++ dashboard.py
@@ -3,6 +3,6 @@ def dashboard():
         print("\nExpense Management System\n")
         print("1. Create Expense")
         print("2. List Expenses")
-        print("3. Generate Report")
+        print("3. Generate Report File")
         print("4. Exit")
         choice = input("Enter your choice: ")
