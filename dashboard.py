86276753bb25bee96acc45d84d4565b3a4eb78e9
Faisal Ashfaq <faisal0ashfaq@gmail.com>
2023-07-01 15:46:15 +0530

    Adding Dashboard File

---
+++ dashboard.py
@@ -0,0 +1,8 @@
+def dashboard():
+    while True:
+        print("\nExpense Management System\n")
+        print("1. Create Expense")
+        print("2. List Expenses")
+        print("3. Generate Report")
+        print("4. Exit")
+        choice = input("Enter your choice: ")
